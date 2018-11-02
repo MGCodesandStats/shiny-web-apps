6a5bc1eb038791242fb95773d84c27164a1c5a9a
Michael Grogan <19137429+MGCodesandStats@users.noreply.github.com>
2018-11-02 00:05:18 +0000

    Update server.R

--- maps/server.R
+++ maps/server.R
@@ -29,6 +29,14 @@ shinyServer(function(input,output) {
       coord_fixed(1.3) + scale_x_continuous(breaks = seq(-200, 200, 20)) + scale_y_continuous(breaks = seq(-100, 100, 10))
     gg1
     
+    boundaries <- data.frame(
+      latbound1 = input$latbound1,
+      longbound1 = input$longbound1,
+      latbound2 = input$latbound2,
+      longbound2 = input$longbound2,
+      stringsAsFactors = FALSE
+    )
+    
     coors <- data.frame(
       lat = input$lat,
       long = input$long,
@@ -40,7 +48,7 @@ shinyServer(function(input,output) {
     gg1 + 
       geom_point(data=coors, aes(long, lat), colour="red", size=2) + 
       ggtitle("World Map") +
-      geom_text_repel(data=coors, aes(long, lat, label=locations)) + xlim(-200,200) + ylim(-100,100)
+      geom_text_repel(data=coors, aes(long, lat, label=locations)) + xlim(input$longbound1,input$longbound2) + ylim(input$latbound1,input$latbound2)
     
   })
   
