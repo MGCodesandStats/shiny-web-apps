58a427aa21cdb361ca241858b4bf4da831cb9441
Michael Grogan <19137429+MGCodesandStats@users.noreply.github.com>
2024-03-09 21:47:56 +0000

    Update ihg_shiny.R

--- ihg/ihg_shiny.R
+++ ihg/ihg_shiny.R
@@ -3,7 +3,7 @@
 library(shiny)
 library(plotly)
 
-# Data sourced from: https://www.ihgplc.com/en/-/media/ihg/files/results/2023/q3-2023/ihg-q3-trading-update-2023-supplementary-information-v2.pdf?la=en&hash=2553202D6D3E26B770763E0BCDB0A402
+# Data sourced from: https://www.ihgplc.com/~/media/Files/I/Ihg-Plc/investors/results/2023/q3-2023/ihg-q3-trading-update-2023-supplementary-information-v2.pdf
 
 brand<-c("InterContinental","Kimpton","Hotel Indigo","Crowne Plaza","EVEN Hotels","Holiday Inn Express","Holiday Inn","Staybridge Suites","Candlewood Suites")
 adr<-c(230.53,256.93,186.12,141.82,166.60,136.76,132.08,136.38,103.21)
@@ -13,7 +13,11 @@ rooms<-c(15694,10886,9685,27392,2744,227533,111835,31225,33156)
 data<-data.frame(revpar,adr,occupancy,rooms)
 
 ui <- fluidPage(
-  headerPanel('IHG Q3 2023: Hotel Metrics'),
+  h1('IHG Q3 2023: Hotel Metrics'),
+  h4('The purpose of this visualisation is to allow users to analyse key hotel KPIs across IHG hotel brands. Data sourced from IHG Q3 2023 earnings report.'),
+  h5('ADR = Average Daily Rate, RevPAR = Revenue per available room.'),
+  a("IHG Q3 2023 earnings data available here", href="https://www.ihgplc.com/~/media/Files/I/Ihg-Plc/investors/results/2023/q3-2023/ihg-q3-trading-update-2023-supplementary-information-v2.pdf"),
+  h5(''),
   sidebarPanel(
     selectInput('xcol','X Variable', names(data)),
     selectInput('ycol','Y Variable', names(data)),
@@ -40,17 +44,15 @@ server <- function(input, output) {
   
   
   output$plot <- renderPlotly(
-    plot1 <- plot_ly(
-      x = x(),
-      y = y(), 
-      type = 'scatter',
-      mode = 'markers',
-      size = size(),
-      color = ~brand,
-      colors = 'Paired',
-      marker = list(opacity = 0.5, sizemode = 'diameter'))
-  )
-  
+  p <- plot_ly(
+    x = x(), 
+    y = y(),  
+    type = 'scatter', 
+    mode = 'markers', 
+    size = size(),
+    color = ~brand, 
+    colors = 'Paired',
+    marker = list(opacity = 0.5, sizemode = 'diameter')))
 }
 
 shinyApp(ui,server)
